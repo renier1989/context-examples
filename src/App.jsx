e43e6a6fe793023ef03578dd73b384a9dc11356c
Renier Vargas <renier1989@gmail.com>
2023-08-17 19:00:32 -0500

    Class133-part2 - intentando optimizar con memo y useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { createContext, useContext, useState } from "react"
+/* eslint-disable react/display-name */
+import { createContext, memo, useCallback, useContext, useState } from "react"
 
 const Contexto = createContext();
 
 const MiProvider = ({children}) => {
   const [contador , setContador] = useState(0)
 
-  const incrementar = ()=>setContador(contador + 1);
-  const decrementar = ()=>setContador(contador - 1);
+  // aqui se intenta aplicar la optimizacion para evitar el re-render de la funciones que son las que actuializan constantemente el estado de la propiedad valor
+  const incrementar = useCallback(()=>setContador(x => x + 1), []);
+  const decrementar = useCallback(()=>setContador(x=>x - 1),[]);
 
   return (
     <Contexto.Provider value={{contador, incrementar, decrementar}}>
@@ -15,21 +17,23 @@ const MiProvider = ({children}) => {
   )
 }
 
-const Incrementar = () => {
+// aqui se intenta optimizar con memo el componente para igualmente evitar el re-rendering
+const Incrementar = memo(() => {
   const {incrementar} = useContext(Contexto)
   console.log('Incrementar');
   return (
     <button onClick={incrementar}> Incrementar</button>
   );
-}
+})
 
-const Decrementar = () => {
+// aqui se intenta optimizar con memo el componente para igualmente evitar el re-rendering
+const Decrementar = memo(() => {
   const {decrementar} = useContext(Contexto)
   console.log('Decrementar');
   return (
     <button onClick={decrementar}> Decrementar</button>
   );
-}
+})
 
 const Title = () => {
   const {contador } = useContext(Contexto);
@@ -53,3 +57,7 @@ function App() {
 export default App
 
 //PARTE 1: "el problema" en esta version de App, se pone en evidencia el problema que tiene Contexto. que es que no se podra evitar de ninguna menera el re-render de los componentes hijos que contento el Provider.
+
+//PARTE 2: "Intento de optimizacion" en esta version se agrego la parte de la optimizacion con memo para los componentes y useCallback para las funciones e intentar memoizar la aplicacion, pero sin un resultado favorable ya que el provider simpre a estar aplicarndo re-rendering a todos los componentes hijos que este contenga. 
+
+// RECOMENTADIONES : usar la API de Context es muy util y flexible si se trata de aplicaciones muy pequeñas sin un gran inpacto sobre la aplicacion, pero para aplicacion mucho mas grandes que van a escalar con el timepo o con analisis de datos y ejecuciones de funciones muy pesadas es recomendable usar redux.
